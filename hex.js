da9a12371f02507be06189eecaf277b7866a7a58
Marina Muñoz <marinacerejidoo@gmail.com>
2023-09-23 19:46:12 +0200

    first commit

---
+++ hex.js
@@ -0,0 +1,23 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 9, "A", "B", "C", "D", "E", "F"];
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+const hexBtn = document.getElementById("hex");
+const simpleBtn = document.getElementById("simple");
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    let randomNumber = Math.floor(Math.random() * hex.length);
+    hexColor += hex[randomNumber];
+  }
+  document.body.style.background = hexColor;
+  color.innerHTML = hexColor;
+});
+
+hexBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  hexBtn.innerHTML = "👇🏼Hex";
+  simpleBtn.innerHTML = "Simple";
+});
